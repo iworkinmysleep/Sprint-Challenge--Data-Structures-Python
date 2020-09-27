3a49a0e639f2a00eaef49e0ffee0df089e3130ff
Jason Hinton <jason_hinton@att.net>
2020-09-26 20:17:12 -0400

    buffer completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = -1
+        self.items = []
+                    
 
     def append(self, item):
-        pass
+        if len(self.items) == self.capacity:
+            self.current = (self.current + 1) % self.capacity
+            self.items[self.current] = item
+        else:
+            self.items.append(item)
+
 
     def get(self):
-        pass
+        return self.items
