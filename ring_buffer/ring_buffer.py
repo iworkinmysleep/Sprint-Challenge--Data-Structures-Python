dbb596c765147238a1c0abb63bc7e619890c0aa4
Jason Hinton <jason_hinton@att.net>
2020-09-27 00:34:38 -0400

    added notes

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,14 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = -1
+        self.index = 0
         self.items = []
                     
 
     def append(self, item):
+        # if array is full, manipulate the index of the item before appending
         if len(self.items) == self.capacity:
-            self.current = (self.current + 1) % self.capacity
-            self.items[self.current] = item
+            self.items[self.index] = item
+            self.index = (self.index + 1) % self.capacity
         else:
             self.items.append(item)
 
